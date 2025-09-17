f67f453a7d2a9f7e18912cbd964ffc1c97437259
luotao21 <luotao21@foxmail.com>
2025-09-17 18:17:53 +0800

    删除多余的字符

--- src/components/ui/footer1.tsx
+++ src/components/ui/footer1.tsx
@@ -56,7 +56,7 @@ const Footer1 = ({
     url: "https://www.atale.com",
     src: "/atale-logo.svg",
     alt: "Atale Logo",
-    title: "Atale",
+    // title: "Atale",
   },
 }: Footer1Props) => {
   return (
